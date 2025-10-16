d27455b87b2261818f03397560932868dfd93a45
Himansith2004 <as20240514@sci.sjp.ac.lk>
2025-10-17 01:29:36 +0530

    Add function to enter cties and display them

--- main.c
+++ main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define Max_city 50
+#define Max_character 40
+
+int takingCityName(char cityName[Max_city][Max_character]);
+void displayCities(char cityName[Max_city][Max_character], int count);
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char cityName[Max_city][Max_character];
+    int cityCount=takingCityName(cityName);
+   displayCities(cityName,cityCount);
+}
+
+int takingCityName(char cityName[Max_city][Max_character])
+{
+    int count=0;
+    char choice;
+
+    for(int i=0;i<Max_city;i++)
+    {
+        printf("Enter the %d city: ",i+1);
+        fgets(cityName[i],Max_character,stdin);
+        cityName[i][strcspn(cityName[i], "\n")] = '\0';
+        count++;
+        printf("Enter \"y\" if you want to and another city otherwise \"n\": ");
+        scanf(" %c",&choice);
+        if(choice == 'n')
+        {
+            printf("All the cities have been entered.");
+            break;
+        }
+        else if((choice !='y')&&(choice !='n'))
+        {
+            printf("THE ENTERED NUMBER is not correct!!");
+            break;
+        }
+        getchar();
+    }
+    return count;
+}
+
+void displayCities(char cityName[Max_city][Max_character], int count)
+{
+    printf("\nList of Cities:\n");
+    for (int i = 0; i < count; i++) {
+        printf("%d. %s\n", i + 1, cityName[i]);
+    }
 }
